45f31af31b051f75bd83cbe8b1adf1a8cd105239
nir.godin <nir.godin@planckresolution.com>
2023-04-25 11:24:36 +0300

    initialized

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MinDistanceRangeSlider from './components/MinDistanceRangeSlider';
+import MultipleSelectChip from './components/MultiSelectDropdown';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <MinDistanceRangeSlider
+          minDistance={10}
+          value={[0, 100]}
+          label={'Popularity'}
         >
-          Learn React
-        </a>
+        </MinDistanceRangeSlider>
+        <MultipleSelectChip label={'Genre'}></MultipleSelectChip>
       </header>
     </div>
   );
