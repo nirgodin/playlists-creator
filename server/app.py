b921aa85f8a3104969bc9550e9519a30f6c5c03d
nir.godin <nir.godin@planckresolution.com>
2023-05-05 10:21:12 +0300

    Updated app and gitignore

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
 from flask_restful import Api
 
 from server.app_resources.configuration_resource import Configuration
@@ -11,11 +11,17 @@ from server.app_resources.prompt_resource import Prompt
 from server.consts.env_consts import SPOTIPY_CLIENT_SECRET
 from server.logic.openai_adapter import OpenAIAdapter
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/build', static_url_path='')
 app.secret_key = os.environ[SPOTIPY_CLIENT_SECRET]
 CORS(app)
 api = Api(app)
 openai_adapter = OpenAIAdapter()
+
+
+@app.route('/')
+@cross_origin
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 
 api.add_resource(Configuration, '/api/configuration')
@@ -25,4 +31,4 @@ api.add_resource(PossibleValues, '/api/possibleValues/<string:column_name>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
