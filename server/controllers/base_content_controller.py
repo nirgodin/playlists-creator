a02db09fcf628e5bca0200b5c900d80203a76f10
nir.godin <nir.godin@planckresolution.com>
2023-05-12 12:59:41 +0300

    Improved flow to enable usage of refresh token

--- server/controllers/base_content_controller.py
+++ server/controllers/base_content_controller.py
@@ -6,6 +6,7 @@ from typing import Dict, List, Optional
 from flask import jsonify, Response
 from flask_restful import Resource
 
+from server.consts.api_consts import ACCESS_TOKEN, REFRESH_TOKEN
 from server.consts.app_consts import ACCESS_CODE, IS_SUCCESS, PLAYLIST_LINK, PLAYLIST_DETAILS, MESSAGE
 from server.consts.data_consts import URI
 from server.data.query_condition import QueryCondition
@@ -20,16 +21,16 @@ class BaseContentController(Resource, ABC):
         self._playlists_creator = PlaylistsCreator()
 
     def _generate_response(self, body: dict, query_conditions: List[QueryCondition]) -> Response:
-        headers = self._build_spotify_headers(body)
-        if headers is None:
-            return self._build_authentication_failure_response()
-
         uris = self._data_filterer.filter(query_conditions)
         if not uris:
             return self._build_no_content_response()
 
+        access_code = body[ACCESS_CODE]
         playlist_details = body[PLAYLIST_DETAILS]
-        playlist_link = self._playlists_creator.create(uris, headers, playlist_details)
+        playlist_link = self._create_playlist(access_code, playlist_details, uris)
+        if playlist_link is None:
+            return self._build_authentication_failure_response()
+
         response = {
             IS_SUCCESS: True,
             PLAYLIST_LINK: playlist_link
@@ -37,17 +38,23 @@ class BaseContentController(Resource, ABC):
 
         return jsonify(response)
 
-    @staticmethod
-    def _build_spotify_headers(body: dict) -> Optional[Dict[str, str]]:
-        access_code = body[ACCESS_CODE]
-        bearer_token = AccessTokenGenerator.generate(access_code)
+    def _create_playlist(self, access_code: str, playlist_details: dict, uris: List[str]) -> Optional[str]:  # TODO: Add retries
+        response = AccessTokenGenerator.generate(access_code)
+        bearer_token = response[ACCESS_TOKEN]
 
-        if bearer_token is not None:
-            return {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {bearer_token}"
-            }
+        if bearer_token is None:
+            return
+
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {bearer_token}"
+        }
+
+        try:
+            return self._playlists_creator.create(uris, headers, playlist_details)
+        except:
+            return self._create_playlist(response[REFRESH_TOKEN], playlist_details, uris)
 
     @staticmethod
     def _build_authentication_failure_response() -> Response:
