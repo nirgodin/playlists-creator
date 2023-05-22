3d3ba5132cfd31146ac7bc2e01c6f3a8de3787fd
nir.godin <nir.godin@planckresolution.com>
2023-05-22 21:26:31 +0300

    First query conditions than track details

--- server/controllers/prompt_controller.py
+++ server/controllers/prompt_controller.py
@@ -3,15 +3,18 @@ from typing import List, Optional, TypeVar
 
 from flask import Response, request
 
-from server.consts.app_consts import PLAYLIST_DETAILS, PROMPT
+from server.consts.app_consts import PLAYLIST_DETAILS, PROMPT, ACCESS_CODE
 from server.consts.data_consts import URI
 from server.consts.openai_consts import QUERY_CONDITIONS_PROMPT_PREFIX_FORMAT, QUERY_CONDITIONS_PROMPT_SUFFIX_FORMAT, \
     TRACKS_AND_ARTISTS_NAMES_PROMPT_FORMAT
 from server.controllers.base_content_controller import BaseContentController
 from server.data.query_condition import QueryCondition
+from server.data.spotify_grant_type import SpotifyGrantType
+from server.logic.access_token_generator import AccessTokenGenerator
 from server.logic.openai.columns_details_creator import ColumnsDetailsCreator
 from server.logic.openai.openai_adapter import OpenAIAdapter
 from server.logic.openai.track_details import TrackDetails
+from server.logic.playlist_cover_photo_creator import PlaylistCoverPhotoCreator
 from server.logic.spotify_tracks_collector import SpotifyTracksCollector
 from server.utils import build_prompt
 
@@ -28,15 +31,18 @@ class PromptController(BaseContentController):
     def post(self) -> Response:
         body = request.get_json()
         user_text = body[PLAYLIST_DETAILS][PROMPT]
-        track_details_uris = self._generate_uris_from_tracks_details(user_text)
+        query_conditions_uris = self._generate_uris_from_query_conditions(user_text)
 
-        if track_details_uris is not None:
-            return self._generate_response(body, track_details_uris)
-        else:
-            query_conditions_uris = self._generate_uris_from_tracks_details(user_text)
+        if query_conditions_uris is not None:
             return self._generate_response(body, query_conditions_uris)
+        else:
+            track_details_uris = self._generate_uris_from_tracks_details(user_text)
+            return self._generate_response(body, track_details_uris)
+        # response = AccessTokenGenerator.generate(grant_type=SpotifyGrantType.AUTHORIZATION_CODE, access_code=body[ACCESS_CODE])
+        # headers = self._build_request_headers(response)
+        # PlaylistCoverPhotoCreator().put_playlist_cover(headers=headers, playlist_id='0tp13KbV1k1qoGzgfTpa36', prompt=body[PLAYLIST_DETAILS][PROMPT])
 
-    def _generate_uris_from_query_conditions(self, user_text: str) -> Optional[List[QueryCondition]]:
+    def _generate_uris_from_query_conditions(self, user_text: str) -> Optional[List[str]]:
         prompt = self._build_query_conditions_prompt(user_text)
         json_serialized_response = self._openai_adapter.fetch_openai(prompt, retries_left=1)
         query_conditions = self._serialize_openai_response(json_serialized_response, klazz=QueryCondition)
