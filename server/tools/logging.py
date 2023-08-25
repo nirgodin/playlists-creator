bb7e5a2a19ff67e823ebb0c63b4e9c1326ce46d8
nir.godin <nir.godin@planckresolution.com>
2023-08-25 12:07:34 +0300

    Fix unprinting logger

--- server/tools/logging.py
+++ server/tools/logging.py
@@ -2,13 +2,12 @@ import logging
 
 
 def get_logger():
-    logger_ = logging.getLogger("PlaylistsCreatorLogger")
-    log_format = logging.Formatter("[%(threadName)s] %(asctime)s [%(levelname)s]  %(message)s")
-    stream_handler = logging.StreamHandler()
-    stream_handler.setFormatter(log_format)
-    logger_.addHandler(stream_handler)
-
-    return logger_
+    logging.basicConfig(
+        format="[%(threadName)s] %(asctime)s [%(levelname)s]  %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+    return logging.getLogger("PlaylistsCreatorLogger")
 
 
 logger = get_logger()
