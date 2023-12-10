ef5e7d0af10e3bcc54b659c2009b9f68a976f624
nir.godin <nir.godin@planckresolution.com>
2023-12-10 09:54:52 +0200

    Remove redundant consts

--- server/consts/openai_consts.py
+++ server/consts/openai_consts.py
@@ -1,22 +1,2 @@
-# URLs
-BASE_OPENAI_API_URL = 'https://api.openai.com/v1'
-CHAT_COMPLETIONS_URL = f'{BASE_OPENAI_API_URL}/chat/completions'
-IMAGES_GENERATION_URL = f'{BASE_OPENAI_API_URL}/images/generations'
-IMAGES_VARIATIONS_URL = f'{BASE_OPENAI_API_URL}/images/variations'
-EMBEDDINGS_URL = f'{BASE_OPENAI_API_URL}/embeddings'
-
-# Body params
-MODEL = 'model'
-MESSAGES = 'messages'
-CHOICES = 'choices'
 CONTENT = 'content'
-IMAGE_SIZE_512 = "512x512"
-DATA = 'data'
 URL = 'url'
-CREATED = 'created'
-N = 'n'
-SIZE = 'size'
-IMAGE = 'image'
-ADA_EMBEDDINGS_MODEL = 'text-embedding-ada-002'
-EMBEDDING = 'embedding'
-INPUT = 'input'
