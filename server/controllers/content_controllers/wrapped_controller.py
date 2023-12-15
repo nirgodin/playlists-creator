7282a44c93db1e967938b7d460c742056da56436
nir.godin <nir.godin@planckresolution.com>
2023-12-15 22:12:25 +0200

    Enabled WrappedController to receive parameters from frontend

--- server/controllers/content_controllers/wrapped_controller.py
+++ server/controllers/content_controllers/wrapped_controller.py
@@ -1,7 +1,9 @@
+from genie_common.utils import safe_nested_get
 from spotipyio import SpotifyClient
 from spotipyio.logic.collectors.top_items_collectors.items_type import ItemsType
 from spotipyio.logic.collectors.top_items_collectors.time_range import TimeRange
 
+from server.consts.app_consts import PLAYLIST_DETAILS, TIME_RANGE
 from server.consts.data_consts import URI, ITEMS
 from server.controllers.content_controllers.base_content_controller import BaseContentController
 from server.data.playlist_resources import PlaylistResources
@@ -12,9 +14,10 @@ class WrappedController(BaseContentController):
                                            request_body: dict,
                                            dir_path: str,
                                            spotify_client: SpotifyClient) -> PlaylistResources:
+        raw_time_range = safe_nested_get(request_body, [PLAYLIST_DETAILS, TIME_RANGE], TimeRange.SHORT_TERM.value)
         response = await spotify_client.current_user.top_items.run(
             items_type=ItemsType.TRACKS,
-            time_range=TimeRange.MEDIUM_TERM,
+            time_range=TimeRange(raw_time_range),
             limit=50
         )
         uris = [item[URI] for item in response[ITEMS]]
