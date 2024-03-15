13b3449f4a76b40ff3089f91e7f05cbc78a07754
nir.godin <nir.godin@planckresolution.com>
2024-03-15 19:46:42 +0200

    Working version with progressing interval

--- client/src/pages/PostSendPage.jsx
+++ client/src/pages/PostSendPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 // import LoadingSpinner from "../components/LoadingSpinner";
 import Confetti from "../components/Confetti";
 import BackToMainPageButton from "../components/BackToMainPageButton";
@@ -7,48 +7,65 @@ import PropTypes from "prop-types";
 import { sendGetRequest } from "../utils/RequestsUtils";
 import CircularProgressWithLabel from "../components/CircularProgressWithLabel";
 
+const STAGE_PROGRESS_MAPPING = {
+  ["playlist_details"]: 40,
+  ["tracks"]: 70,
+  ["cover"]: 90,
+};
+
+const STAGE_TEXT_MAPPING = {
+  ["playlist_details"]: "Selecting tracks",
+  ["tracks"]: "Creating playlist cover",
+  ["cover"]: "Almost there!",
+};
+
 function PostSendPage(props) {
-  const stageProgressMapping = {
-    ["playlist_details"]: 20,
-    ["tracks"]: 50,
-    ["cover"]: 90,
-  };
+  let intervalNumber = useRef(1);
   const [progress, setProgress] = useState(10);
+  const [text, setText] = useState("Request received!")
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      if (!(props.isSuccessful)) {
-        pollCaseProgress();
-      } else {
-        clearInterval(intervalId);
-        handlePlaylistLink();
-      }
-    }, 3000);
-  }, [props.isSuccessful]);
-
-  async function pollCaseProgress() {
+  const pollCaseProgress = useCallback(async () => {
     const route = `cases/${props.caseId}/progress`;
     const caseStatus = await sendGetRequest(route, "caseStatus");
-    const progress = stageProgressMapping[caseStatus];
+    const currentProgress = STAGE_PROGRESS_MAPPING[caseStatus];
 
-    if (progress !== undefined) {
-      setProgress(progress);
+    if (currentProgress !== undefined) {
+      if (currentProgress > progress) {
+        setProgress(currentProgress);
+      }
+
+      setText(STAGE_TEXT_MAPPING[caseStatus]);
     }
     props.setIsSuccessful((caseStatus === "completed"));
-  }
+  }, [props])
 
-  async function handlePlaylistLink() {
+  const handlePlaylistLink = useCallback(async () => {
     const route = `cases/${props.caseId}/playlist`;
     const playlistId = await sendGetRequest(route, "playlistId");
     const playlistLink = `https://open.spotify.com/playlist/${playlistId}`;
     props.setPlaylistLink(playlistLink);
-  }
+  }, [props])
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (props.isSuccessful) {
+        clearInterval(intervalId);
+        handlePlaylistLink();
+      } else if (intervalNumber.current % 3 === 0) {
+        pollCaseProgress();
+      } else {
+        setProgress((previousProgress) => previousProgress + 1);
+      }
+
+      intervalNumber.current = intervalNumber.current + 1;
+    }, 1000);
+  }, [props.isSuccessful, handlePlaylistLink, pollCaseProgress]);
 
   if (!(props.isSuccessful && props.playlistLink !== "")) {
     return (
       <header className="spinner-container">
         <div>
-          <p>Loading</p>
+          <p>{text}</p>
           <CircularProgressWithLabel value={progress} />
         </div>
       </header>
