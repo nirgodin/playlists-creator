c3c66ea90327a52148218f6136083131d7daea86
nir.godin <nir.godin@planckresolution.com>
2024-03-28 19:58:00 +0200

    Integrate redis testkit and created test for cached token generator

--- tests/server/integration/test_resources.py
+++ tests/server/integration/test_resources.py
@@ -1,8 +1,9 @@
 from typing import Optional
 
+from aioredis import Redis
 from genie_common.utils import random_alphanumeric_string
 from genie_datastores.postgres.testing import PostgresMockFactory
-from genie_testkit import PostgresTestkit
+from genie_testkit import PostgresTestkit, RedisTestkit
 from sqlalchemy.ext.asyncio import AsyncEngine
 from starlette.testclient import TestClient
 
@@ -15,21 +16,27 @@ class TestResources:
     def __init__(self,
                  client: TestClient = TestClient(app),
                  postgres_testkit: PostgresTestkit = PostgresTestkit(),
+                 redis_testkit: RedisTestkit = RedisTestkit(),
                  mock_factory: PostgresMockFactory = PostgresMockFactory(),
                  username: str = random_alphanumeric_string(),
                  password: str = random_alphanumeric_string(),
-                 engine: Optional[AsyncEngine] = None):
+                 engine: Optional[AsyncEngine] = None,
+                 redis: Optional[Redis] = None):
         self.client = client
         self.postgres_testkit = postgres_testkit
+        self.redis_testkit = redis_testkit
         self.mock_factory = mock_factory
         self.username = username
         self.password = password
         self.auth = (username, password)
         self.engine = engine
+        self.redis = redis
 
     def __enter__(self) -> "TestResources":
         self.postgres_testkit.__enter__()
+        self.redis_testkit.__enter__()
         self.engine = self.postgres_testkit.get_database_engine()
+        self.redis = self.redis_testkit.get_redis()
         app.dependency_overrides[get_authenticator] = lambda: Authenticator(
             username=self.username,
             password=self.password
@@ -39,4 +46,6 @@ class TestResources:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.engine = None
+        self.redis = None
         self.postgres_testkit.__exit__(exc_type, exc_val, exc_tb)
+        self.redis_testkit.__exit__(exc_type, exc_val, exc_tb)
---
+++ tests/server/integration/tools/test_cached_token_generator.py
@@ -0,0 +1,99 @@
+from random import randint
+from typing import Dict
+from unittest.mock import AsyncMock, patch
+
+from _pytest.fixtures import fixture
+from aioredis import Redis
+from genie_common.encoders import GzipJsonEncoder
+from genie_common.utils import random_alphanumeric_string, random_string_array
+from spotipyio import AccessTokenGenerator
+from spotipyio.consts.api_consts import ACCESS_TOKEN, REFRESH_TOKEN
+from spotipyio.logic.authentication.spotify_grant_type import SpotifyGrantType
+
+from server.tools.cached_token_generator import CachedTokenGenerator
+from tests.server.integration.test_resources import TestResources
+
+
+class TestCachedTokenGenerator:
+    @fixture(autouse=True, scope="class")
+    def set_up(self, resources: TestResources):
+        with patch("genie_datastores.redis.redis_client.get_redis") as mock_get_redis:
+            mock_get_redis.return_value = resources.redis
+            yield
+
+    async def test_generate__no_cache__calls_spotify_and_sets_key(self,
+                                                                  token_generator: CachedTokenGenerator,
+                                                                  access_token_generator: AsyncMock,
+                                                                  resources: TestResources,
+                                                                  encoder: GzipJsonEncoder,
+                                                                  response: Dict[str, str]):
+        access_code = random_alphanumeric_string()
+
+        actual = await token_generator.generate(SpotifyGrantType.AUTHORIZATION_CODE, access_code)
+
+        assert actual == response
+        assert access_token_generator.generate.call_count == 1
+        await self._assert_key_stored_in_cache(
+            redis=resources.redis,
+            encoder=encoder,
+            access_code=access_code,
+            response=response
+        )
+
+    async def test_create__with_cache__doesnt_call_spotify(self,
+                                                           resources: TestResources,
+                                                           encoder: GzipJsonEncoder,
+                                                           response: Dict[str, str],
+                                                           token_generator: CachedTokenGenerator,
+                                                           access_token_generator: AsyncMock):
+        access_code = await self._given_cached_access_code(resources.redis, encoder, response)
+
+        actual = await token_generator.generate(SpotifyGrantType.AUTHORIZATION_CODE, access_code)
+
+        assert actual == response
+        assert access_token_generator.generate.call_count == 0
+
+    @fixture(scope="class")
+    def encoder(self) -> GzipJsonEncoder:
+        return GzipJsonEncoder()
+
+    @fixture(scope="function")
+    def token_generator(self, access_token_generator: AsyncMock) -> CachedTokenGenerator:
+        return CachedTokenGenerator(access_token_generator)
+
+    @fixture(scope="class")
+    def response(self) -> Dict[str, str]:
+        scopes = random_string_array()
+        return {
+            ACCESS_TOKEN: random_alphanumeric_string(),
+            "token_type": random_alphanumeric_string(),
+            "scope": " ".join(scopes),
+            "expires_in": randint(0, 3000),
+            REFRESH_TOKEN: random_alphanumeric_string(),
+        }
+
+    @fixture(scope="function")
+    def access_token_generator(self, response: Dict[str, str]) -> AsyncMock:
+        mock_token_generator = AsyncMock(AccessTokenGenerator)
+        mock_token_generator.generate.return_value = response
+
+        yield mock_token_generator
+
+        mock_token_generator.reset_mock()
+
+    @staticmethod
+    async def _assert_key_stored_in_cache(redis: Redis,
+                                          encoder: GzipJsonEncoder,
+                                          access_code: str,
+                                          response: Dict[str, str]) -> None:
+        cache_response = await redis.get(access_code)
+        decoded_response = encoder.decode(cache_response)
+        assert decoded_response == response
+
+    @staticmethod
+    async def _given_cached_access_code(redis: Redis, encoder: GzipJsonEncoder, response: Dict[str, str]) -> str:
+        access_code = random_alphanumeric_string()
+        encoded_response = encoder.encode(response)
+        await redis.set(access_code, encoded_response)
+
+        return access_code
