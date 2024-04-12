d39702dcbe8cbe641957ddd38c65e767c8ab77e8
nir.godin <nir.godin@planckresolution.com>
2024-04-12 12:22:50 +0300

    Added PHOTO case status

--- server/data/case_status.py
+++ server/data/case_status.py
@@ -5,6 +5,7 @@ class CaseStatus(Enum):
     CREATED = "created"
     COMPLETED = "completed"
     COVER = "cover"
+    PHOTO = "photo"
     PLAYLIST = "playlist"
     PLAYLIST_DETAILS = "playlist_details"
     TEXTUAL_QUERY = "textual_query"
