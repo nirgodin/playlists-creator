173f423e7108b5ad33382732733fdbf40ee1cb1a
nir.godin <nir.godin@planckresolution.com>
2024-04-13 16:18:48 +0300

    Removed case_id from chat_completions_request

--- server/data/chat_completions_request.py
+++ server/data/chat_completions_request.py
@@ -5,7 +5,6 @@ from server.consts.openai_consts import CONTENT, ROLE, USER_ROLE
 
 @dataclass
 class ChatCompletionsRequest:
-    case_id: str
     prompt: str
     start_char: str
     end_char: str
