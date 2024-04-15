e0e3a761c39a56f03c27fe84e93a5a3f0eb0e787
nir.godin <nir.godin@planckresolution.com>
2024-04-15 19:28:20 +0300

    Deleted event_loop fixture which causes issues with test teardown

--- tests/server/conftest.py
+++ tests/server/conftest.py
@@ -1,6 +1,3 @@
-from asyncio import AbstractEventLoop, new_event_loop
-from typing import Generator
-
 from _pytest.fixtures import fixture
 from aiohttp import ClientSession
 from aioresponses import aioresponses
@@ -8,12 +5,9 @@ from genie_common.openai import OpenAIClient
 from spotipyio import SpotifyClient
 from spotipyio.logic.authentication.spotify_session import SpotifySession
 
-
-@fixture(scope="session")
-def event_loop() -> Generator[AbstractEventLoop, None, None]:
-    loop = new_event_loop()
-    yield loop
-    loop.close()
+from server.component_factory import get_artists_searcher
+from server.logic.ocr.artists_searcher import ArtistsSearcher
+from server.logic.openai.openai_adapter import OpenAIAdapter
 
 
 @fixture(scope="session")
@@ -41,3 +35,13 @@ def mock_responses() -> aioresponses:
 @fixture(scope="session")
 def openai_client(client_session: ClientSession) -> OpenAIClient:
     return OpenAIClient.create(client_session)
+
+
+@fixture(scope="class")
+def openai_adapter(openai_client: OpenAIClient) -> OpenAIAdapter:
+    return OpenAIAdapter(openai_client)
+
+
+@fixture(scope="class")
+def artists_searcher() -> ArtistsSearcher:
+    return get_artists_searcher()
