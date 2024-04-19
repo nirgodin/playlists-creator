e0f4b0838aeb68e89820f5cfb15cd550d3054736
nir.godin <nir.godin@planckresolution.com>
2024-04-19 12:29:16 +0300

    Check milvus on CI

--- server/controllers/health_controller.py
+++ server/controllers/health_controller.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 from typing import Callable, Awaitable, Dict
 
+from aiohttp import ClientSession
 from aioredis import Redis
 from fastapi import HTTPException
 from genie_common.tools import logger
@@ -11,9 +12,10 @@ from starlette.responses import JSONResponse
 
 
 class HealthController:
-    def __init__(self, db_engine: AsyncEngine, redis: Redis):
+    def __init__(self, db_engine: AsyncEngine, redis: Redis, session: ClientSession):
         self._db_engine = db_engine
         self._redis = redis
+        self._session = session
 
     async def check_server_health(self) -> JSONResponse:
         for resource_name, check_func in self._health_checks.items():
@@ -33,5 +35,13 @@ class HealthController:
     def _health_checks(self) -> Dict[str, Callable[[], Awaitable[None]]]:
         return {
             "Postgres": lambda: execute_query(engine=self._db_engine, query=text("SELECT 1")),
-            "Redis": lambda: self._redis.ping()
+            "Redis": lambda: self._redis.ping(),
+            "Milvus": self._check_milvus_health
         }
+
+    async def _check_milvus_health(self):
+        async with self._session.get("http://localhost:19530/v1/vector/collections") as raw_response:
+            raw_response.raise_for_status()
+            res = await raw_response.json()
+
+        return res
--- tests/server/integration/controllers/test_health_controller.py
+++ tests/server/integration/controllers/test_health_controller.py
@@ -1,6 +1,8 @@
+from functools import partial
 from http import HTTPStatus
 
 from _pytest.fixtures import fixture
+from aiohttp import ClientSession
 from aioredis import Redis
 from genie_common.utils import random_postgres_connection_url, random_port, random_alphanumeric_string
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
@@ -12,12 +14,12 @@ from tests.server.integration.test_resources import TestResources
 
 
 class TestHealthController:
-    async def test_check_server_health__is_healthy__returns_200(self, resources: TestResources):
-        resources.app.dependency_overrides[get_health_controller] = lambda: HealthController(
-            db_engine=resources.engine,
-            redis=resources.redis
+    async def test_check_server_health__is_healthy__returns_200(self, resources):
+        resources.app.dependency_overrides[get_health_controller] = partial(
+            self._get_health_controller,
+            resources.engine,
+            resources.redis
         )
-
         response = resources.client.get(url="/health", auth=resources.auth)
 
         assert response.status_code == HTTPStatus.OK.value
@@ -26,9 +28,10 @@ class TestHealthController:
     async def test_check_server_health__postgres_unhealthy__returns_503(self,
                                                                         resources: TestResources,
                                                                         non_existing_db_engine: AsyncEngine):
-        resources.app.dependency_overrides[get_health_controller] = lambda: HealthController(
-            db_engine=non_existing_db_engine,
-            redis=resources.redis
+        resources.app.dependency_overrides[get_health_controller] = partial(
+            self._get_health_controller,
+            non_existing_db_engine,
+            resources.redis
         )
 
         response = resources.client.get(url="/health", auth=resources.auth)
@@ -39,9 +42,10 @@ class TestHealthController:
     async def test_check_server_health__redis_unhealthy__returns_503(self,
                                                                      resources: TestResources,
                                                                      non_existing_redis: Redis):
-        resources.app.dependency_overrides[get_health_controller] = lambda: HealthController(
-            db_engine=resources.engine,
-            redis=non_existing_redis
+        resources.app.dependency_overrides[get_health_controller] = partial(
+            self._get_health_controller,
+            resources.engine,
+            non_existing_redis
         )
 
         response = resources.client.get(url="/health", auth=resources.auth)
@@ -49,12 +53,20 @@ class TestHealthController:
         assert response.status_code == HTTPStatus.SERVICE_UNAVAILABLE.value
         assert response.json() == {"detail": "Could not connect to `Redis`"}
 
-    @fixture(scope="class")
+    async def _get_health_controller(self, db_engine, redis):
+        async with ClientSession() as client_session:
+            yield HealthController(
+                db_engine=db_engine,
+                redis=redis,
+                session=client_session
+            )
+
+    @fixture
     def non_existing_db_engine(self) -> AsyncEngine:
         url = random_postgres_connection_url()
         return create_async_engine(url=url, poolclass=NullPool)
 
-    @fixture(scope="class")
+    @fixture
     def non_existing_redis(self) -> Redis:
         return Redis(
             host=random_alphanumeric_string(),
